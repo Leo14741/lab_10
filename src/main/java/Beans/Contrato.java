fd3822e666665f075e70088be703f32b78c5659a
Dana Nolasco <a20202330@pucp.edu.pe>
2022-11-16 20:34:24 -0500

    Beans de Contrato y Credenciales

--- src/main/java/Beans/Contrato.java
+++ src/main/java/Beans/Contrato.java
@@ -1,4 +1,50 @@
 package Beans;
 
 public class Contrato {
+
+    private String nroDeContrato;
+    private String idCliente;
+    private String divisa;
+    private String estado;
+    private int mesesEnEseEstado;
+
+    public String getNroDeContrato() {
+        return nroDeContrato;
+    }
+
+    public void setNroDeContrato(String nroDeContrato) {
+        this.nroDeContrato = nroDeContrato;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getDivisa() {
+        return divisa;
+    }
+
+    public void setDivisa(String divisa) {
+        this.divisa = divisa;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getMesesEnEseEstado() {
+        return mesesEnEseEstado;
+    }
+
+    public void setMesesEnEseEstado(int mesesEnEseEstado) {
+        this.mesesEnEseEstado = mesesEnEseEstado;
+    }
 }
--- src/main/java/Beans/Credentials.java
+++ src/main/java/Beans/Credentials.java
@@ -1,4 +1,23 @@
 package Beans;
 
 public class Credentials {
+
+    private String numeroDocumento;
+    private int tipoUsuario;
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public int getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(int tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
 }
