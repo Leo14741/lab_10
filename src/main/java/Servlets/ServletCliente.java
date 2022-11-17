873033ef433065e689c78a25177b76f493a35a6b
leonardo <a20201696@pucp.edu.pe>
2022-11-16 21:20:46 -0500

    Dao y Servlet de cliente

--- src/main/java/Servlets/ServletCliente.java
+++ src/main/java/Servlets/ServletCliente.java
@@ -1,15 +1,35 @@
 package Servlets;
 
+import Beans.Cliente;
+import Daos.ClienteDaos;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "ServletCliente", value = "/ServletCliente")
 public class ServletCliente extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String action = request.getParameter("action");
+        action = (action == null) ? "listar" : action;
+        RequestDispatcher requestDispatcher;
+        ClienteDaos daoClientes = new ClienteDaos();
+        ArrayList<Cliente> list = daoClientes.listarClientes();
+
+        switch (action){
+            case "listar":
+                request.setAttribute("lista", daoClientes.listarClientes());
+
+                requestDispatcher = request.getRequestDispatcher("clientes/lista.jsp");
+                requestDispatcher.forward(request, response);
+            case "crear":
+                requestDispatcher = request.getRequestDispatcher("clientes/formCrear.jsp");
+                requestDispatcher.forward(request,response);
+        }
 
     }
 
