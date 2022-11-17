5dc8074eaf346db0d4f949f8f1886f79c48ac7bb
Dana Nolasco <a20202330@pucp.edu.pe>
2022-11-16 21:04:14 -0500

    Daos de Contrato con listarContratos y mostrarCantidadContratos (donde solo falta hacer el sql con la consulta correspondiente)

--- src/main/java/Daos/ContratoDaos.java
+++ src/main/java/Daos/ContratoDaos.java
@@ -1,4 +1,63 @@
 package Daos;
 
-public class ContratoDaos {
+import Beans.Contrato;
+import Dtos.cantidadContratosDto;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class ContratoDaos extends DaoBase{
+
+    public ArrayList<Contrato> listarContratos() {
+        ArrayList<Contrato> listaDeContratos = new ArrayList<>();
+        //DaoSelecciones daoSelecciones = new DaoSelecciones();
+        //DaoArbitros daoArbitros = new DaoArbitros();
+
+        try (Connection conn = this.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery( "SELECT * FROM lab9.partidos;");){
+
+            while (rs.next()) {
+                Contrato contrato = new Contrato();
+
+                contrato.setNroDeContrato(rs.getString(1));
+                contrato.setIdCliente(rs.getString(2));
+                contrato.setDivisa(rs.getString(3));
+                contrato.setEstado(rs.getString(4));
+                contrato.setMesesEnEseEstado(Integer.parseInt(rs.getString(5)));
+
+                listaDeContratos.add(contrato);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return listaDeContratos;
+    }
+
+    public ArrayList<cantidadContratosDto> mostrarCantidadContratos(){
+        ArrayList<cantidadContratosDto> lista = new ArrayList<>();
+
+        String sql= "";
+
+        try (Connection conn = this.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while(rs.next()){
+                cantidadContratosDto cant = new cantidadContratosDto();
+                cant.setContratosEstado0(Integer.parseInt(rs.getString(1)));
+                cant.setContratosEstado1(Integer.parseInt(rs.getString(2)));
+                cant.setContratosEstado2(Integer.parseInt(rs.getString(3)));
+                lista.add(cant);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return lista;
+
+    }
+
 }
---
+++ src/main/java/Dtos/cantidadContratosDto.java
@@ -0,0 +1,32 @@
+package Dtos;
+
+public class cantidadContratosDto {
+
+    private int contratosEstado0;
+    private int contratosEstado1;
+    private int contratosEstado2;
+
+    public int getContratosEstado0() {
+        return contratosEstado0;
+    }
+
+    public void setContratosEstado0(int contratosEstado0) {
+        this.contratosEstado0 = contratosEstado0;
+    }
+
+    public int getContratosEstado1() {
+        return contratosEstado1;
+    }
+
+    public void setContratosEstado1(int contratosEstado1) {
+        this.contratosEstado1 = contratosEstado1;
+    }
+
+    public int getContratosEstado2() {
+        return contratosEstado2;
+    }
+
+    public void setContratosEstado2(int contratosEstado2) {
+        this.contratosEstado2 = contratosEstado2;
+    }
+}
