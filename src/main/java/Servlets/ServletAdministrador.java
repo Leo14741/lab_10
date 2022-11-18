76183c309ce64adf6db447bbe1f3c41842ec8550
Dana <a20202330@pucp.edu.pe>
2022-11-17 23:20:13 -0500

    Servlet de Administrador

--- src/main/java/Servlets/ServletAdministrador.java
+++ src/main/java/Servlets/ServletAdministrador.java
@@ -6,10 +6,19 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet(name = "ServletAdministrador", value = "/ServletAdministrador")
+@WebServlet(name = "ServletAdministrador", urlPatterns = {"/AdministradorServlet","/AdminServlet","/Admin","/Administrador"})
 public class ServletAdministrador extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action") == null ? "crearCliente" : request.getParameter("action");
+        RequestDispatcher requestDispatcher;
+
+        switch (action){
+            case "crearCliente":
+                requestDispatcher = request.getRequestDispatcher("/Administrador/CrearCliente.jsp");
+                requestDispatcher.forward(request, response);
+        }
+
 
     }
 
